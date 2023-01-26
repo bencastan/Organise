c51c9c33b46e52ba31dd34ecdd67a2d7ad94831c
Ben Castan <ben@castanfamily.com>
2023-01-26 04:16:46 +0000

    Bug fixes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time
 import shutil
 import os
-from watchdog.observers import observer
-from watchdog.event import FileSystemEventHandler
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-DOWNLOADS_DIR = '/User/benc/Downloads'
-MUSIC_DIR = '/Users/benc/Music'
-IMAGES_DIR = '/Users/benc/Images'
-DOCUMENTS_DIR = '/Users/benc/Documents'
-OTHER_DIR = '/Users/benc/Other'
+DOWNLOADS_DIR = '/home/benc/Downloads'
+MUSIC_DIR = '/home/benc/Music'
+IMAGES_DIR = '/home/benc/Images'
+DOCUMENTS_DIR = '/home/benc/Documents'
+OTHER_DIR = '/home/benc/Other'
 
 # File types to match
 MUSIC_FILES_TYPES = ['.mp3', '.flac']
@@ -19,26 +19,26 @@ class FileMover(FileSystemEventHandler):
     def on_modified(self, event):
         for file in os.listdir(DOWNLOADS_DIR):
             file_path = os.path.join(DOWNLOADS_DIR, file)
-            if os.path.isfile(file_path)
+            if os.path.isfile(file_path):
             # Check file type and move to appropriate directory
-            if file.endswith(tuple(MUSIC_FILES_TYPES)):
-                shutil.move(file_path, MUSIC_DIR)
-            elif file.emdswith(tuple(IMAGE_FILE_TYPES)):
-                shutil.move(file_path, IMAGES_DIR)
-            elif file.endswith(tuple(DOCUMENT_FILE_TYPE)):
-                shutil.move(file_path,DOCUMENTS_DIR)
-            else:
-                shutil.move(file_path,OTHER_DIR)
+                if file.endswith(tuple(MUSIC_FILES_TYPES)):
+                    shutil.move(file_path, MUSIC_DIR)
+                elif file.endswith(tuple(IMAGE_FILE_TYPES)):
+                    shutil.move(file_path, IMAGES_DIR)
+                elif file.endswith(tuple(DOCUMENT_FILE_TYPE)):
+                    shutil.move(file_path,DOCUMENTS_DIR)
+                else:
+                    shutil.move(file_path,OTHER_DIR)
 
 observer = Observer()
-event_handler = FIleMover()
+event_handler = FileMover()
 observer.schedule(event_handler, DOWNLOADS_DIR, recursive=True)
 observer.start()
 
 try:
     while True:
         time.sleep(1)
-except KeyboardInterupt:
+except KeyboardInterrupt:
     observer.stop()
 observer.join
 
